de7de5c1e1d772ca39a65d2d8cdb71daece1204c
= <=>
2022-06-20 14:54:13 -0300

    se arreglaron las indentaciones

--- airflow/dags/DAG_UniversidadNacionaldeJujuy.py
+++ airflow/dags/DAG_UniversidadNacionaldeJujuy.py
@@ -1,20 +1,19 @@
 from datetime import datetime
-from airflow import DAG 
-from airflow.operators.dummy  import DummyOperator
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
 """
-configuration of the DAG without queries or processing
-for National University of Jujuy
+configuration of the DAG without queries or processing for University of Palermo
 """
-with DAG (
-          'DAG_Universidad_Nacional_de_Jujuy',
-          description='DAG para la Universidad Nacional de Jujuy',
-          #execute each one hour
-          scheduler_interval="@hourly", 
-          start_date=datetime(2022,6,19)
-          ) as dag:
-                #only the tasks of extrancting data , transforming them and uploading them are declare  
-                extract_task=DummyOperator(task_id='extract_task')
-                transform_task=DummyOperator(task_id='transform_task')
-                load_task=DummyOperator(task_id='transform_task')
-                #the execution order of the DAG
-                extract_task >> transform_task >> load_task             
+with DAG(
+        'DAG_Universidad_de_Palermo',
+        description='DAG para la Universidad Nacional de Jujuy',
+        # execute each one hour
+        schedule_interval="@hourly", 
+        start_date=datetime(2022, 6, 19)
+        ) as dag:
+        # Data extraction, transformation and loading tasks are declared
+            extract_task = DummyOperator(task_id='extract_task')
+            transform_task = DummyOperator(task_id='transform_task')
+            load_task = DummyOperator(task_id='load_task')
+        # the execution order of the DAG
+            extract_task >> transform_task >> load_task
