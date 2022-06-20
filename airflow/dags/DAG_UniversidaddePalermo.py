3da3d30e0dbc4f9c84b3e2d207fce4db9f1b5e1f
= <=>
2022-06-20 10:36:31 -0300

    se sacaron espacios de mas

--- airflow/dags/DAG_UniversidaddePalermo.py
+++ airflow/dags/DAG_UniversidaddePalermo.py
@@ -12,10 +12,9 @@ with DAG(
         schedule_interval= "@hourly", 
         start_date=datetime(2022,6,19)
         ) as dag:
-        #only the tasks of extrancting data , transforming them and uploading them are declare
+            #only the tasks of extrancting data , transforming them and uploading them are declare
             extract_task=DummyOperator(task_id='extract_task')
             transform_task=DummyOperator(task_id='transform_task')
             load_task=DummyOperator(task_id='load_task')
-        #the execution order of the DAG
-            extract_task >> transform_task >> load_task
-            
+            #the execution order of the DAG
+            extract_task >> transform_task >> load_task         
