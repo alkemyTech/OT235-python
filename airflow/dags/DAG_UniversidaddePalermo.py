3c5b08415960f29e61cc964a99fba2cec7f8bfa2
= <=>
2022-06-20 12:34:51 -0300

    mas correcciones archivo Universidad de Palermo

--- airflow/dags/DAG_UniversidaddePalermo.py
+++ airflow/dags/DAG_UniversidaddePalermo.py
@@ -4,16 +4,16 @@ from airflow.operators.dummy import DummyOperator
 """
 configuration of the DAG without queries or processing for University of Palermo
 """
-with DAG(
+with DAG (
         'DAG_Universidad_de_Palermo',
         description='DAG para la Universidad de Palermo',
         # execute each one hour
         schedule_interval="@hourly", 
         start_date=datetime(2022, 6, 19)
-        ) as dag:
+         ) as dag:
                 # only the tasks of extrancting data , transforming them and uploading them are declare
-                extract_task=DummyOperator (task_id='extract_task')
-                transform_task=DummyOperator (task_id='transform_task')
-                load_task=DummyOperator (task_id='load_task')
+                extract_task=DummyOperator(task_id='extract_task')
+                transform_task=DummyOperator(task_id='transform_task')
+                load_task=DummyOperator(task_id='load_task')
                 # the execution order of the DAG
                 extract_task >> transform_task >> load_task
