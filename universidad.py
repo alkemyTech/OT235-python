4e33e54cc345fec88018e02498e8f9356f3fd534
Nicolas Ezequiel Lugones <lugones.nicolas@gmail.com>
2022-06-22 16:58:57 -0300

    Add logs

--- universidad.py
+++ universidad.py
@@ -3,17 +3,17 @@ from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 
-
+#   Configuracion del logger
 logging.basicConfig(
-    level=logging.INFO, 
-    datefmt='%Y-%m-%d', 
+    filename='main.log'
+    level=logging.INFO,  
     format='%(asctime)s %(name)s %(message)s'
 )
 logger = logging.getLogger('Universidades_G')
 
 logger.info('ETL DAG para el grupo de universidades G')
 
-
+""" Definicion del DAG """
 with DAG(
     'universidades',
     owner= 'lugones_nicolas'
@@ -24,8 +24,17 @@ with DAG(
     start_date=datetime(2022, 6, 20),
 
 ) as dag:
-    extract = DummyOperator(task_id='extract')
-    transform = DummyOperator(task_id='transform')
-    load = DummyOperator(task_id='load')
+    extract_u1 = DummyOperator(task_id='extract_u1')
+    transform_u1 = DummyOperator(task_id='transform_u1')
+    load_u1 = DummyOperator(task_id='load_u1')
 
-    extract >> load >> load
+    extract_u2 = DummyOperator(task_id='extract_u2')
+    transform_u2 = DummyOperator(task_id='transform_u2')
+    load_u2 = DummyOperator(task_id='load_u2')
+
+#   Tareas a realizar
+    extract_u1 >> load_u1 >> load_u1
+    logger.info('Se corrio el ETL Facultad Latinoamericana De Ciencias Sociales')
+
+    logger.info('Se corrio el ETL Universidad J. F. Kennedy')
+    extract_u2 >> load_u2 >> load_u2
