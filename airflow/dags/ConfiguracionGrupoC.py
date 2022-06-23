75c31d964174a87276f0189b29196b47f131b7af
= <=>
2022-06-23 19:27:41 -0300

    Se agrego default_args al dag

--- airflow/dags/ConfiguracionGrupoC.py
+++ airflow/dags/ConfiguracionGrupoC.py
@@ -1,14 +1,38 @@
 from datetime import datetime
+from datetime import timedelta
 
 from airflow import DAG
 
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+
+import logging
+
+# configuration of logs
+def my_logs() :
+    logging.basicConfig(format='%(asctime)s - %(name)s  - %(message)s',
+    datefmt='%Y-%m-%d',
+    level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+
+# configuration of retries
+default_args = {
+'owner': 'airflow',
+'retries': 5,
+'retry_delay': timedelta(minutes=5)
+}
 
 with DAG(
     'DAG_Universidad',
     description='DAG para la Universidad',
+    default_args=default_args,
     # execute each one hour
     schedule_interval="@hourly", 
     start_date=datetime(2022, 6, 23)
     ) as dag:
-        pass
+        #logging task
+        logging_task = PythonOperator(task_id='logging_task',
+        python_callable=my_logs) 
+
+        logging_task
