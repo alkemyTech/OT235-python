74a55bcd76459160fc1ac7304eaf51d9bfd31ac4
= <=>
2022-06-24 15:30:33 -0300

    OT235-70

--- airflow/dags/ConfiguracionGrupoC.py
+++ airflow/dags/ConfiguracionGrupoC.py
@@ -1,17 +1,18 @@
-from datetime import datetime,timedelta
-
 from airflow import DAG
-
 from airflow.operators.python import PythonOperator
-
+from datetime import datetime, timedelta
 import logging
 
+# data normalization
+def transform():
+    pass
+
 # configuration of logs
-def my_logs() :
+def my_logs():
     logging.basicConfig(format='%(asctime)s - %(name)s  - %(message)s',
     datefmt='%Y-%m-%d',
     level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
+    logger= logging.getLogger(__name__)
 
 
 # configuration of retries
@@ -29,6 +30,8 @@ with DAG(
     schedule_interval="@hourly", 
     start_date=datetime(2022, 6, 23)
     ) as dag:
-        #logging task
-        logging_task = PythonOperator(task_id='logging_task',python_callable=my_logs)
-        logging_task
+    # logging task
+    logging_task = PythonOperator(task_id='logging_task', python_callable=my_logs)
+    # normalization task
+    transform_task = PythonOperator(task_id='transform_task', python_callable=transform)
+    logging_task >> transform_task
