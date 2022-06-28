c5ada72ed3dbc0e286521612460d8a8469611723
Nicolas Ezequiel Lugones <lugones.nicolas@gmail.com>
2022-06-27 22:07:03 -0300

    Add consulta.sql review

--- consulta.sql
+++ consulta.sql
@@ -1,3 +1,7 @@
-select university,career,inscription_date,first_name,last_name,gender,age,postal_code,"location",email from 
+-- datos sin normaliazar
 
-select university,career,inscription_date,first_name,last_name,gender,age,postal_code,"location",email from 
+SELECT universities,careers,inscription_dates,"names",sexo,birth_dates,locations,emails FROM lat_sociales_cine
+WHERE inscription_dates BETWEEN CAST('01/9/2020' AS DATE) AND CAST('01/02/2021' AS DATE);
+
+
+-- select universidades,carreras,fechas_de_inscripcion,nombres,sexo,fechas_nacimiento,codigos_postales,emails from uba_kenedy;
