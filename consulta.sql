df8d4ef25d7600d8a73d3922e7884e5e89d62006
Nicolas Ezequiel Lugones <lugones.nicolas@gmail.com>
2022-06-28 15:53:59 -0300

    query sql review

--- consulta.sql
+++ consulta.sql
@@ -1,7 +1,27 @@
--- datos sin normaliazar
+SELECT  universities,
+        careers,
+        inscription_dates AS inscription_date,
+	    SPLIT_PART("names",'-',2) AS last_name,
+	    SPLIT_PART("names",'-',1) AS first_name,
+        sexo AS gender,
+	    EXTRACT(YEAR FROM age(TIMESTAMP 'NOW()',TO_DATE(birth_dates,'DD-MM-YYYY'))) AS age,
+	    locations AS "location",
+        emails AS email
+FROM lat_sociales_cine 
+WHERE universities LIKE '-FACULTAD-LATINOAMERICANA-DE-CIENCIAS-SOCIALES'
+AND inscription_dates BETWEEN '01-02-2021' AND '01-09-2020';
 
-SELECT universities,careers,inscription_dates,"names",sexo,birth_dates,locations,emails FROM lat_sociales_cine
-WHERE inscription_dates BETWEEN CAST('01/9/2020' AS DATE) AND CAST('01/02/2021' AS DATE);
-
-
--- select universidades,carreras,fechas_de_inscripcion,nombres,sexo,fechas_nacimiento,codigos_postales,emails from uba_kenedy;
+SELECT  universidades as universities,
+        carreras as careers,
+        fechas_de_inscripcion AS inscription_date,
+	    SPLIT_PART(nombres,'-',2) AS last_name,
+	    SPLIT_PART(nombres,'-',1) AS first_name,
+        sexo AS gender,
+ 	    fechas_nacimiento,
+-- 	    locations AS "location",
+		codigos_postales as postal_code,
+        emails AS email
+FROM uba_kenedy 
+WHERE universidades LIKE 'universidad-j.-f.-kennedy';
+-- AND fechas_de_inscripcion BETWEEN '01-02-2021' AND '01-09-2020';
+-- Falta corregir las fechas de la segunda universidad y agregar columna location
