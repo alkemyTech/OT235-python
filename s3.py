20cb5bd9da27e01323b99a6762d1477c7af2c24d
Seba <sebastiancifuentes1978@gmail.com>
2022-06-29 15:14:33 -0300

    OT235-98y99 corregido

--- s3.py
+++ s3.py
@@ -5,26 +5,29 @@ from airflow.hooks.S3_hook import S3Hook
 import pandas as pd
 import logging
 
-def upload_to_s3(filename: str,  bucket_name: str) -> None:
+#Genero el hook para subir el archivo al bucket
+def subir_al_s3_universidades_h(filename: str,  bucket_name: str) -> None:
     hook = S3Hook('s3_conn')
     hook.load_file(filename=filename, key=key, bucket_name=bucket_name)
 
 
 with DAG(
-	's3',
-	description='subir_s3',
+	'subir_al_s3_universidades_h',
+	description='subir_al_s3_universidades_h',
 	schedule_interval='@hourly',
 	start_date=datetime(2022, 6, 21),
 ) as dag:
-    tarea_3=PythonOperator(
-        task_id='upload_to_s3',
-        python_callable=upload_to_s3,
-        op_kwargs={'filename':'../files/u_cine.csv','key':'u_cine.csv','bucket_name':'cohorte-junio-a192d78b'}
+    tarea_1=PythonOperator(
+        task_id='subir_al_s3_universidades_h',
+        python_callable=subir_al_s3_universidades_h,
+        #los parametros opcionales definen que archivo y con que nombre lo subo
+        op_kwargs={'filename':'../files/u_cine_universidades_h.csv','key':'u_cine_universidades_h.csv','bucket_name':'cohorte-junio-a192d78b'}
     )
-    tarea_4=PythonOperator(
-        task_id='upload_to_s3_2',
-        python_callable=upload_to_s3,
-        op_kwargs={'filename':'../files/uba.csv','key':'uba.csv','bucket_name':'cohorte-junio-a192d78b'}
+    tarea_2=PythonOperator(
+        task_id='subir_al_s3_universidades_h_2',
+        python_callable=subir_al_s3_universidades_h,
+        #los parametros opcionales definen que archivo y con que nombre lo subo
+        op_kwargs={'filename':'../files/uba_universidades_h.csv','key':'uba_universidades_h.csv','bucket_name':'cohorte-junio-a192d78b'}
     )
 
-    tarea_3 >> tarea_4
+    tarea_1 >> tarea_2
