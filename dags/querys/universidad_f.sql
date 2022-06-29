332697e222c78fdcf93eb703cd11462bd12de321
Cristian Rosas <rosascristian26@gmail.com>
2022-06-28 23:11:48 -0300

    OT235-12 Escribir el código de dos consultas SQL, una para cada universidad. para el grupo de universidades A

---
+++ dags/querys/universidad_f.sql
@@ -0,0 +1,10 @@
+-- Universidad flores (No cuenta con la columna localización)
+SELECT universidad as university,
+       carrera as career,
+       fecha_de_inscripcion as inscription_date,
+       name,
+       fecha_nacimiento as age,
+       codigo_postal as postal_code,
+       correo_electronico as email
+from flores_comahue
+WHERE TO_DATE(fecha_de_inscripcion,'yyyy-mm-dd') BETWEEN '2020-09-01' AND '2021-02-01';
---
+++ dags/querys/universidad_v_m.sql
@@ -0,0 +1,10 @@
+-- Universidad vialla maria (no cuenta con la columna codigo posta)
+SELECT universidad as university,
+       carrera as career,
+       fecha_de_inscripcion as inscription_date,
+       nombre as name,
+       fecha_nacimiento as age,
+       localidad as location,
+       email as email
+from salvador_villa_maria
+WHERE TO_DATE(fecha_de_inscripcion,'dd-mon-yy') BETWEEN '2020-09-01' AND '2021-02-01';
