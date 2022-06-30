ad0d4a5368bcf79056c821b6ad77ee55d46baa57
= <=>
2022-06-30 10:36:45 -0300

    OT235-14 Modificaciones al los archivos SQL

--- SQL_GrupoC/Universidad_Nacional_de_Jujuy.sql
+++ SQL_GrupoC/Universidad_Nacional_de_Jujuy.sql
@@ -1,16 +1,13 @@
-WITH refining_data as(
 select university,
 	   career,
 	   to_date(inscription_date,'YYYY-MM-DD') as inscription_date,
-	   nombre as names,
+	   nombre as full_name,
 	   sexo as gender,
 	   to_date(birth_date,'YYYY-MM-DD') as birth_date,
-	   location,
+	   location as locations,
 	   email
 from jujuy_utn
-where university like 'universidad nacional de jujuy')
-select * 
-from refining_data
-where inscription_date between '2020-09-01' and '2021-02-01'
+where university like 'universidad nacional de jujuy'
+and to_date(inscription_date,'YYYY-MM-DD') between '2020-09-01' and '2021-02-01'
 	   
 	   
