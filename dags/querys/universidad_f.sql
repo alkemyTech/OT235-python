ce471af8d84d33fb4d90612a69784713a02bc1c4
Cristian Rosas <rosascristian26@gmail.com>
2022-07-06 01:22:31 -0300

    OT235-12 Constulas SQL Fix (Filtrado nombre de universidad)

--- dags/querys/universidad_f.sql
+++ dags/querys/universidad_f.sql
@@ -2,9 +2,10 @@
 SELECT universidad as university,
        carrera as career,
        fecha_de_inscripcion as inscription_date,
-       name,
+       name as name,
+       sexo as gender,
        fecha_nacimiento as age,
        codigo_postal as postal_code,
        correo_electronico as email
 FROM flores_comahue
-WHERE TO_DATE(fecha_de_inscripcion,'yyyy-mm-dd') BETWEEN '2020-09-01' AND '2021-02-01';
+WHERE universidad like '%FLORES%' OR universidad like '%flores%' AND TO_DATE(fecha_de_inscripcion,'yyyy-mm-dd') BETWEEN '2020-09-01' AND '2021-02-01';
--- dags/querys/universidad_v_m.sql
+++ dags/querys/universidad_v_m.sql
@@ -3,8 +3,9 @@ SELECT universidad as university,
        carrera as career,
        fecha_de_inscripcion as inscription_date,
        nombre as name,
+       sexo as gender,
        fecha_nacimiento as age,
        localidad as location,
        email as email
 FROM salvador_villa_maria
-WHERE TO_DATE(fecha_de_inscripcion,'dd-mon-yy') BETWEEN '2020-09-01' AND '2021-02-01';
+WHERE universidad like '%MARÍA%' OR universidad like '%maría%' AND TO_DATE(fecha_de_inscripcion,'dd-mon-yy') BETWEEN '2020-09-01' AND '2021-02-01';
