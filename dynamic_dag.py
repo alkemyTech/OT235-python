b37cedb96c9956be9d390468b4326f463b742c42
Nicolas Ezequiel Lugones <lugones.nicolas@gmail.com>
2022-07-14 14:45:46 -0300

    Correccion dag dinamico

--- dynamic_dag.py
+++ dynamic_dag.py
@@ -4,11 +4,9 @@ from pathlib import Path
 
 """Create Yml path and create DAG from here"""
 yml_path_1= Path(r'/home/lnicolas/acceleracion_alkemy/config_file.yml')
-yml_path_2= Path(r'/home/lnicolas/acceleracion_alkemy/config_file_2.yml')
 
 
 dag_factory = dagfactory.DagFactory(yml_path)
-dag_factory = dagfactory.DagFactory(yml_path_2)
 
 
 dag_factory.clean_dags(globals())
