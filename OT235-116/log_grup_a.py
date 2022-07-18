1f00aef56e1c27bfafec888d605140967ad55b5d
Cristian Rosas <rosascristian26@gmail.com>
2022-07-18 14:27:34 -0300

    OT235-116 Arreglo del formato de .conf a .cfg, y ajusten en el formato de los logs

--- OT235-116/log_grup_a.py
+++ OT235-116/log_grup_a.py
@@ -3,7 +3,7 @@ import logging.handlers
 from os import path
 
 # We specify the working path since logging works from the root not where the .py file is executed
-log_file_path = path.join(path.dirname(path.abspath(__file__)), 'log_config_a.conf')
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'log_config_a.cfg')
 logging.config.fileConfig(log_file_path)
 
 logger = logging.getLogger('root')
